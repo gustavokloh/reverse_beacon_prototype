4234b0d5360ab5c95a18fb42ef132f7798700e63
Gustavo Kloh <gustavo.kloh@dtmconsultoria.com>
2014-03-05 21:55:54 -0300

    Allow to set PORT as environment variable

--- server.js
+++ server.js
@@ -6,5 +6,7 @@ app.configure(function () {
 })
 
 // listen (start app with node server.js) ======================================
-app.listen(8080);
-console.log("<<<<<<<<<<<< App listening on port 8080");
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
